9f1e1a27c763785fe9973addc62a34a161d0167a
Seph-23 <seongmlyu95@gmail.com>
2022-09-28 09:23:46 +0900

    menu bar and community side bar started

--- secondboard/src/main/java/myweb/secondboard/controller/BoardController.java
+++ secondboard/src/main/java/myweb/secondboard/controller/BoardController.java
@@ -3,6 +3,7 @@ package myweb.secondboard.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -10,5 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/boards")
 @RequiredArgsConstructor
 public class BoardController {
+
+  @GetMapping("/home")
+  public String home() {
+    return "/boards/boardHome";
+  }
 
 }
